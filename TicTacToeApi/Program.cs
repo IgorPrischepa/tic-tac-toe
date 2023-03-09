1cc0fa420520152d5e905308e023989d1899a722
Igor Prischepa <5076626+IgorPrischepa@users.noreply.github.com>
2023-03-09 16:29:29 +0300

    Add jwt service and db package

--- TicTacToeApi/Program.cs
+++ TicTacToeApi/Program.cs
@@ -1,11 +1,68 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using TicTacToeApi.Dal.Db;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//Configuring DB
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new ArgumentNullException("Default connections string can't be null.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+//Auth
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:audience"],
+        ValidIssuer = builder.Configuration["Jwt:issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:secret"]!))
+    };
+});
+
 // Add services to the container.
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", _ => { _.RequireRole("Admin"); });
+    options.AddPolicy("Player", _ => { _.RequireRole("Player"); });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("JWT Bearer", new OpenApiSecurityScheme
+    {
+        Description = "This is a JWT bearer authentication scheme",
+        In = ParameterLocation.Header,
+        Scheme = "Bearer",
+        Type = SecuritySchemeType.Http
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme{
+            Reference = new OpenApiReference{
+                Id = "JWT Bearer",
+                Type = ReferenceType.SecurityScheme
+            }
+            }, new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -18,6 +75,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
